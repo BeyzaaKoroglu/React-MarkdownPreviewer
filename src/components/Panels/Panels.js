9f3120b90e12d9c5d5f54bd61d1543532c800951
Beyza Köroğlu <83373912+BeyzaaKoroglu@users.noreply.github.com>
2022-10-27 20:43:42 +0300

    react-markdown added

--- src/components/Panels/Panels.js
+++ src/components/Panels/Panels.js
@@ -1,15 +1,14 @@
-import { marked } from "marked";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { changeUserText, setCurrentText } from "../../redux/texts/textsSlice";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { Styled } from "./Panels.styled";
 
 const Panels = () => {
   const dispatch = useDispatch();
   const currentText = useSelector((state) => state.texts.currentText);
   const help = useSelector((state) => state.texts.help);
-  const parsedText = marked(currentText, { sanitize: true });
-  const processedText = { __html: parsedText };
 
   useEffect(() => {
     dispatch(setCurrentText());
@@ -26,10 +25,12 @@ const Panels = () => {
         readOnly={help}
         className="text-panel-left"
       />
-      <div
-        dangerouslySetInnerHTML={processedText}
-        className="text-panel-right"
-      />
+      <div className="text-panel-right">
+        <ReactMarkdown
+          children={`${currentText}`}
+          remarkPlugins={[remarkGfm]}
+        />
+      </div>
     </Styled>
   );
 };
